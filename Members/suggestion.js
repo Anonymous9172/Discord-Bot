dd9fbd36859579e68c874697c367a6efc7c0375d
Leon-J9172 <46303705+Leon-J9172@users.noreply.github.com>
2019-01-21 20:23:01 +0000

    Update suggestion.js

--- Members/suggestion.js
+++ Members/suggestion.js
@@ -1,1 +1,42 @@
+const Commando = require('discord.js-commando');
+const Discord = require('discord.js');
 
+class SuggestionCommand extends Commando.Command 
+  {
+    constructor(client)
+      {
+        super(client,{
+          name: 'report',
+          group: 'members',
+          memberName: 'report',
+          description: 'Report a member or bug.',
+          //userPermissions: ['KICK_MEMBERS']
+    
+       });      
+      }
+  async run(message, args)
+      {
+         let mem = message.mentions.members.first() || message.member; 
+        
+       let words = args.split(' ');  
+     let reason = words.slice(0);
+        
+        var suggestionEmbed = new Discord.RichEmbed()
+       .setColor('#2327ff')
+        .setThumbnail(message.author.avatarURL)
+        .setTitle(`Report`)
+        .addField('Reported By:', '___ ___' + message.author)
+        .addField('Report:', reason) 
+        //.setTimestamp()
+        .setFooter(`Sent by ${message.author.username}.`)
+
+        let incidentschannel = message.guild.channels.find(`name`, "suggestions");
+        
+    if (!incidentschannel) return message.reply("Couldn't find channel `#suggestion` !");
+        
+        message.delete()
+    incidentschannel.send(suggestionEmbed)
+    message.author.send(`${message.author} Your suggestion has been filed! :mailbox_with_mail:`)
+      }
+  }
+    module.exports =  SuggestionCommand;        
